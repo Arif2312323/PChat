c878d792204f0292cdf324a1286efd836baebc8d
Arif2312323 <arifalmas3.14@gmail.com>
2024-06-14 19:23:43 +0530

    Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,14 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
 const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const message_route_1 = __importDefault(require("./routes/message.route"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const socket_1 = require("./socket/socket");
 dotenv_1.default.config();
 const PORT = process.env.PORT || 5001;
-socket_1.app.use((0, cookie_parser_1.default)()); // for parsing cookies
-socket_1.app.use(express_1.default.json()); // for parsing application/json
+socket_1.app.use((0, cookie_parser_1.default)());
+socket_1.app.use(express_1.default.json());
+socket_1.app.use((0, cors_1.default)({
+    origin: '*',
+    credentials: true
+}));
 socket_1.app.use("/api/auth", auth_route_1.default);
 socket_1.app.use("/api/messages", message_route_1.default);
 socket_1.app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
