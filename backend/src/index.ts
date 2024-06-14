d0ce5290bab75546c91f7d2868cbf2da7130468b
Arif2312323 <arifalmas3.14@gmail.com>
2024-06-14 18:55:59 +0530

    Fixed all the deployment issuses

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route";
+import messageRoutes from "./routes/message.route";
+
+import dotenv from "dotenv";
+import { app, server } from "./socket/socket";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(cookieParser()); // for parsing cookies
+app.use(express.json()); // for parsing application/json
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+
+server.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
